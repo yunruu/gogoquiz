67bb140f14bb3c875a89871719d64a554f70d04e
yunruu <yunruuchin@gmail.com>
2025-03-17 20:31:12 +0800

    feat: add navbar ui

---
+++ src/components/navbar.tsx
@@ -0,0 +1,11 @@
+export interface NavbarProps {
+  className?: string;
+}
+
+export default function Navbar({ className }: NavbarProps) {
+  return (
+    <div className={`bg-zinc-50 z-[1000] text-violet-800 font-bold h-14 flex items-center px-4 ${className}`}>
+      myquizmod
+    </div>
+  );
+}
