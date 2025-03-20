5315291dab3f647893b1bb13368a4830142c310b
yunruu <yunruuchin@gmail.com>
2025-03-21 01:29:54 +0800

    feat: create custom toast method to accept error parameter

--- src/components/custom-toast.tsx
+++ src/components/custom-toast.tsx
@@ -1,18 +1,31 @@
-import { Slide, ToastContainer } from "react-toastify";
+import { Slide, toast, ToastContainer } from 'react-toastify';
+
+export interface ICustomToastOptions {
+  type?: 'success' | 'error' | 'info' | 'warning';
+  error?: Error;
+}
+
+export const customToast = (message: string, options: ICustomToastOptions) => {
+  if (options.error) {
+    return toast(`${message}: ${options.error.message}`, { type: 'error' });
+  }
+  return toast(message, { type: options.type });
+};
 
 export default function CustomToast() {
-    return (
-        <ToastContainer
-            position="top-right"
-            autoClose={5000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick={false}
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="light"
-            transition={Slide} />
-    )
+  return (
+    <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick={false}
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+      transition={Slide}
+    />
+  );
 }
