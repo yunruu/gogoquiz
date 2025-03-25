33235d090bd8f9beb728ac2df972841e4ab7056b
yunruu <yunruuchin@gmail.com>
2025-03-25 14:01:58 +0800

    feat: propagate data to dialog if necessary

--- src/components/custom-dialog.tsx
+++ src/components/custom-dialog.tsx
@@ -1,13 +1,12 @@
 import { Cross1Icon } from '@radix-ui/react-icons';
 import { Button, Dialog, IconButton } from '@radix-ui/themes';
+import { useState } from 'react';
 
 export interface ICustomDialogOptions {
   triggerButtonText?: string;
   title: string;
   description: string;
   content: string;
-  isOpen?: boolean;
-  onOpenChange?: (isOpen: boolean) => void;
   triggerButtonStyle?: 'primary' | 'secondary' | 'icon';
   triggerButtonIcon?: React.ReactNode;
   type?: 'success' | 'error' | 'info' | 'warning';
@@ -16,11 +15,21 @@ export interface ICustomDialogOptions {
   onConfirm?: (data: unknown) => void;
   primaryAction?: () => void;
   secondaryAction?: () => void;
+  data?: unknown;
 }
 
 export default function CustomDialog(options: ICustomDialogOptions) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOnConfirm = () => {
+    if (options.onConfirm) {
+      options.onConfirm(options.data);
+    }
+    setIsOpen(false);
+  };
+
   return (
-    <Dialog.Root open={options.isOpen} onOpenChange={options.onOpenChange}>
+    <Dialog.Root open={isOpen} onOpenChange={setIsOpen}>
       <Dialog.Trigger>
         {options.triggerButtonIcon ? (
           <IconButton variant="ghost">{options.triggerButtonIcon}</IconButton>
@@ -41,7 +50,7 @@ export default function CustomDialog(options: ICustomDialogOptions) {
         <div>
           <p>{options.content}</p>
           <div className="flex justify-end gap-4">
-            <Button onClick={options.onConfirm}>Confirm</Button>
+            <Button onClick={handleOnConfirm}>Confirm</Button>
           </div>
         </div>
       </Dialog.Content>
